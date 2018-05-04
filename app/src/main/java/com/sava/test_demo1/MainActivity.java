90104c2e410c91fe1bbe5ac374f3ba8330737a13
sava-vn <levansang.gthn@gmail.com>
2018-05-04 10:02:57 +0700

    Thêm comment

--- app/src/main/java/com/sava/test_demo1/MainActivity.java
+++ app/src/main/java/com/sava/test_demo1/MainActivity.java
@@ -75,6 +75,7 @@ public class MainActivity extends AppCompatActivity {
                 alertDialog.show();
             }
         });
+        //Thêm mới Song
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
